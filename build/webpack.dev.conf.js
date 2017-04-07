f4931c5f6c7db6effb3e17a40034de1fd5ce1ad0
wangxjsteven <525_wxj@163.com>
2017-04-07 19:21:04 +0800

    entry

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,7 +1,40 @@
+var webpack = require('webpack'),
+    path = require('path'),
+    vueRouter = require('vue-router'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
+    entry: {
+        main: path.resolve(__dirname, "../app/main.js"), //已多次提及的唯一入口文件
+        vendor: ['vue', 'vue-router' /*, 'jquery', 'bootstrap'//bootstrap的jquery插件列表，前期只用css，后面跟进需要决定*/ ]
+    },
     output: {
-        path: __dirname + "/public", //打包后的文件存放的地方
-        filename: "bundle.js" //打包后输出文件的文件名
-    }
-}
+        path: path.resolve(__dirname, "../public"), //打包后的文件存放的地方
+        filename: "[name].[chunkhash].js", //打包后输出文件的文件名
+        publicPath: '/'
+    },
+    // resolve: {
+    //     extensions: ['.js', '.vue', '.json'],
+    //     alias: {
+    //         '@': path.resolve(__dirname, '../public/'),
+    //         vue: 'vue/dist/vue.js'
+    //     }
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, '../public/index.html')
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: function(module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        //CommonChunksPlugin will now extract all the vendor modules from vendor and main bundles
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest' //But since there are no more vendor modules between them we end up with just the runtime code included in the manifest file
+        })
+    ]
+};
