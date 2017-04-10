ce3b1b5363e00606915666c7a464ddf5e052093d
wangxjsteven <525_wxj@163.com>
2017-04-10 22:49:43 +0800

    '410'

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,29 +1,41 @@
 var webpack = require('webpack'),
     path = require('path'),
-    vueRouter = require('vue-router'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, "../app/main.js"), //已多次提及的唯一入口文件
-        vendor: ['vue', 'vue-router' /*, 'jquery', 'bootstrap'//bootstrap的jquery插件列表，前期只用css，后面跟进需要决定*/ ]
+        main: path.resolve(__dirname, "../src/main.js"), //已多次提及的唯一入口文件
+        common: ['vue', 'vue-router' /*, 'jquery', 'bootstrap'//bootstrap的jquery插件列表，前期只用css，后面跟进需要决定*/ ]
     },
     output: {
-        path: path.resolve(__dirname, "../public"), //打包后的文件存放的地方
+        path: path.resolve(__dirname, "../dist"), //打包后的文件存放的地方
         filename: "[name].[chunkhash].js", //打包后输出文件的文件名
         publicPath: '/'
     },
-    // resolve: {
-    //     extensions: ['.js', '.vue', '.json'],
-    //     alias: {
-    //         '@': path.resolve(__dirname, '../public/'),
-    //         vue: 'vue/dist/vue.js'
-    //     }
-    // },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            '@': path.resolve(__dirname, '../'),
+            vue: 'vue/dist/vue.js'
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: 'css-loader'
+            })
+        }, {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
+        }]
+    },
     plugins: [
+        new ExtractTextPlugin('styles.css'),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: path.resolve(__dirname, '../public/index.html')
+            template: path.resolve(__dirname, '../src/index.html')
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common',
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '../common/libs/bootstrap/css/bootstrap.css';
+import './css/main.css';
+
+// var Vue = require('vuejs');
+var Vue = require('vue');
+// var VueRouter = require('vue-router').default;
+
+var app = new Vue({
+	el: '#root',
+	data: {}
+});
